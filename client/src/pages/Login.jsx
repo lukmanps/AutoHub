f7e21a7c0aa13fe424a4e3bbb1f5b8b59824bdf6
lukmanps <luqmansha6@gmail.com>
2023-12-14 15:18:03 +0530

    Completed login functionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { handleRegister } from '../services/auth/auth.service';
+import toast from 'react-hot-toast'
 
 const Login = () => {
+  const [loading, setLoading] = useState(false);
+  const {register, handleSubmit, formState: {errors}, getValues} = useForm();
+  const navigate = useNavigate();
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
   return (
-    <div>Signin</div>
+    <div>
+      <div className='text-center my-10'>
+        <h1 className='text-secondary text-3xl font-bold '>Welcome Back!</h1>
+        <span className='text-light-gray font-normal'>Don't have an account?</span>
+        <Link to={'/register'}>
+          <span className='hover: text-slate-800'> Register</span>
+        </Link>
+      </div>
+
+      <div className='max-w-xs sm:max-w-sm mx-auto'>
+        <form className='flex flex-col gap-3' onSubmit={handleSubmit(onSubmit)}>
+
+          <input
+            className='shadow-sm  p-3 rounded-lg focus:shadow-lg outline-none'
+            id='email'
+            type='text'
+            placeholder='Email' 
+            {...register('email', {
+              required: true,
+              pattern: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+            })}/>
+            {errors.email && <p className='text-primary-dark text-sm'>Invalid Email</p>}
+
+          
+
+          <input
+            className='shadow-sm  p-3 rounded-lg focus:shadow-lg outline-none'
+            id='password'
+            type='password'
+            placeholder='Password'
+            {...register('password', {
+              required: 'Invalid Password',
+              minLength: {
+                value: 6,
+                message: 'Password should contain atleast 6 characters'
+              }
+            })} 
+            error = {Boolean(errors.password)}/>
+            {errors.password && <p className='text-primary-dark text-sm'>{errors.password.message}</p>}
+            
+
+      
+          <button 
+          disabled={loading}
+          className='bg-secondary py-3 my-3 text-slate-100 font-medium rounded-lg hover:opacity-90'
+          type='submit'
+          >{loading ? 'Loading...' : 'Login'}</button>
+        </form>
+      </div>
+
+    </div>
   )
 }
 
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,9 @@
 import userCollection from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const signup = async (req, res, next) => {
     console.log(req.body, ' :Data from client');
@@ -12,5 +16,28 @@ export const signup = async (req, res, next) => {
     } catch (err) {
         next(err);
     }
-
 }
+
+export const login = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await userCollection.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User not found!'));
+        const validPassword = await bcrypt.compare(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Wrong Credential'));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_KEY);
+        const {password: pass,__v, updatedAt, ...data} = validUser._doc;
+        console.log(data);
+        res
+        .cookie('access-token', token, {httpOnly: true})
+        .status(200)
+        .json(data)
+    } catch (err) {
+        next(err);
+    }
+}
+
+
+
+
+
